9cdbd2d6fd4b2bd2d18cf362af79b8067a592259
buddhanussati <buddhanussati@gmail.com>
2026-02-22 06:58:02 +0700

    Refactor RSS generation and update file names

--- generate-rss-jayasaro.js
+++ generate-rss-jayasaro.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 // Use unique filenames so we don't overwrite the Thanissaro Bhikkhu feed
 const HISTORY_FILE = 'history-jayasaro.json';
-const RSS_FILE = 'rss-jayasaro.xml';
+const RSS_FILE = 'jayasaro.xml';
 const MAX_ITEMS = 10;
 
 // 1. Read the JS file containing the Ajahn Jayasāro quotes
@@ -55,7 +55,7 @@ fs.writeFileSync(HISTORY_FILE, JSON.stringify(history, null, 2));
 const itemsXml = history.map(item => `
     <item>
       <title><![CDATA[${item.title}]]></title>
-      <link>https://buddhanussati.github.io/dhamma-quotes/</link>
+      <link>https://buddhanussati.github.io/dhamma-quotes</link>
       <description><![CDATA[
         ${item.content}
       ]]></description>
@@ -69,13 +69,13 @@ const rssXml = `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
   <channel>
     <title>Ajahn Jayasāro Quotes</title>
-    <link>https://buddhanussati.github.io/dhamma-quotes/</link>
+    <link>https://buddhanussati.github.io/dhamma-quotes</link>
     <description>Dhamma quotes by Ajahn Jayasāro, updated every 6 hours</description>
     <lastBuildDate>${pubDate}</lastBuildDate>
     <image>
       <url>https://buddhanussati.github.io/dhamma-quotes/favicon.png</url>
       <title>Ajahn Jayasāro Quotes</title>
-      <link>https://buddhanussati.github.io/dhamma-quotes/</link>
+      <link>https://buddhanussati.github.io/dhamma-quotes</link>
     </image>
     ${itemsXml}
   </channel>
@@ -84,3 +84,4 @@ const rssXml = `<?xml version="1.0" encoding="UTF-8" ?>
 fs.writeFileSync(RSS_FILE, rssXml);
 
 console.log(`Generated RSS with ${history.length} items. Latest: ${titleText}`);
+
